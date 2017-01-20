19f7152c2f77063533a76b917cda7fba86116fa3
koencorstjens <koencorstjens@icloud.com>
2017-01-20 12:27:59 +0100

    Initial commit

---
+++ src/main/java/eu/corstjens/SpringBootWsExampleApplication.java
@@ -0,0 +1,13 @@
+package eu.corstjens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWsExampleApplication.class, args);
+	}
+
+}
